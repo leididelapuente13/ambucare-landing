196907d068c4ec46335cec13fbd771c91c9fb4cb
Leidi De La Puente <106483873+leididelapuente13@users.noreply.github.com>
2025-03-18 21:25:32 -0500

    Update route.ts

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -2,6 +2,15 @@ import { connectdb } from "@/lib/database/mongodb";
 import EmailModel from "@/models/Email.model";
 import { NextRequest, NextResponse } from "next/server";
 
+const getByEmail = async (email: string) => {
+    try {
+        const found_user = await EmailModel.findOne({ email });
+        return found_user;
+    } catch (error) {
+        console.error("Error getting user by email", error);
+        return null;
+    }
+}
 
 export const POST = async (req: NextRequest) => {
     try {
@@ -13,7 +22,7 @@ export const POST = async (req: NextRequest) => {
             return NextResponse.json({ message: "Email is required" }, { status: 400 });
         }
 
-        const found_user = await GET_BY_EMAIL(email);
+        const found_user = await getByEmail(email);
         if (found_user) {
             return NextResponse.json({ message: `El email ${email} ya está en nuestra lista.`}, { status: 409 });
         }
@@ -23,17 +32,6 @@ export const POST = async (req: NextRequest) => {
         return NextResponse.json({ message: "Email guardado exitosamente" }, { status: 201 });
     } catch (error) {
         console.error(error);
-        return NextResponse.json({ message: "Internal sever error" }, { status: 500 });
+        return NextResponse.json({ message: "Internal server error" }, { status: 500 });
     }
-}
-
-export const GET_BY_EMAIL = async (email: string) => {
-    try {
-        const found_user = await EmailModel.findOne({ email });
-        return found_user;
-    } catch (error) {
-        console.error("Error getting user by email", error);
-        return null;
-    }
-
 }
